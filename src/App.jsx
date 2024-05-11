8557c22867b6fff7114072314137522d7c43e3a7
StructuredTrading <samowen1992@hotmail.com>
2024-05-11 10:16:11 +1000

    feat: react app init with vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import HomePage from './pages/Home'
+import AboutPage from './pages/About'
+import ContactPage from './pages/Contact'
+
+function App() {
+
+  return (
+    <>
+      <HomePage />
+      <AboutPage />
+      <ContactPage />
+    </>
+  )
+}
+
+export default App
